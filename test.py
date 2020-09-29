23cecc9fbf9f4346a43f5ae44abfe7b2974a9508
MTDAntoineA <72069354+MTDAntoineA@users.noreply.github.com>
2020-09-29 11:28:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def Algorythm () :
-    for i in range(999):
+    for i in range(000,999):
         l = list(str(f'{i:03}'))
         if l[0] != l[1] and l[1] != l[2] and l[2] != l[0] :
             print(f'{i:03}')
